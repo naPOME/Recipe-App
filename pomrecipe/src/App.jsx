a05b5173ed4e59f56bfb03e3a1f75c835a8a7f68
naPOME <natipomebiz1234@gmail.com>
2023-12-24 00:49:37 +0300

    o

--- pomrecipe/src/App.jsx
+++ pomrecipe/src/App.jsx
@@ -1,47 +1,49 @@
-import CircleIcon from "./components/CircleIcon"
-import Search from "./components/Search"
-import { FaCoffee, FaPizzaSlice, FaIceCream, FaApple } from 'react-icons/fa';
+import React from "react";
+import CircleIcon from "./components/CircleIcon";
+import Search from "./components/Search";
 import Popular from "./components/Popular";
-function App() {
-  
+import { FaCoffee, FaPizzaSlice, FaIceCream, FaApple } from "react-icons/fa";
 
+function App() {
   return (
     <>
-<div className="grid items-center gap-6 md:grid-cols-2 lg:grid-cols-4">
-      <div className="flex items-center">
-        <p className="text-3xl font-semibold tracking-tight dark:brightness-150 transition duration-300 transform hover:scale-105 hover:text-white">
-          <span className="text-gray-810">P</span>
-          <span className="text-gray-700">O</span>
-          <span className="text-gray-600">M</span>
-          <span className="text-gray-500">'s </span>
-          <span className="text-gray-810">R</span>
-          <span className="text-gray-600">e</span>
-          <span className="text-gray-700">c</span>
-          <span className="text-gray-800">i</span>
-          <span className="text-gray-600">p</span>
-          <span className="text-gray-700">e</span>
-        </p>
+      <div className="grid items-center gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div className="flex items-center">
+          <p className="text-3xl font-semibold tracking-tight dark:brightness-150 transition duration-300 transform hover:scale-105 hover:text-white">
+            <span className="text-gray-810">P</span>
+            <span className="text-gray-700">O</span>
+            <span className="text-gray-600">M</span>
+            <span className="text-gray-500">'s </span>
+            <span className="text-gray-810">R</span>
+            <span className="text-gray-600">e</span>
+            <span className="text-gray-700">c</span>
+            <span className="text-gray-800">i</span>
+            <span className="text-gray-600">p</span>
+            <span className="text-gray-700">e</span>
+          </p>
+        </div>
       </div>
-    </div>
-    <Search/>
-    <div className="flex justify-center space-x-4">
-      {/* Use CircleIcon with different icons and props for each circle */}
-      <CircleIcon bgColor="rgb(31,41,55)" iconColor="#fff" description="Coffee" icon={<FaCoffee />} />
-      <CircleIcon bgColor="rgb(31,41,55)" iconColor="#fff" description="Pizza" icon={<FaPizzaSlice />} />
-      <CircleIcon bgColor="rgb(31,41,55)" iconColor="#fff" description="Ice Cream" icon={<FaIceCream />} />
-      <CircleIcon bgColor="rgb(31,41,55)" iconColor="#fff" description="Apple" icon={<FaApple />} />
-    </div>
-    <h4 class="mb-2 mt-0 text-3xl font-small leading-tight text-primary px-10 text-start">Popular Meal</h4>
-<div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-0">
-  
-    <Popular />
-    <Popular />
-    <Popular />
-    </div>
-
+      <Search />
+      <div className="flex justify-center space-x-4">
+        {[
+          { bgColor: "rgb(31,41,55)", iconColor: "#fff", description: "Coffee", icon: <FaCoffee /> },
+          { bgColor: "rgb(31,41,55)", iconColor: "#fff", description: "Pizza", icon: <FaPizzaSlice /> },
+          { bgColor: "rgb(31,41,55)", iconColor: "#fff", description: "Ice Cream", icon: <FaIceCream /> },
+          { bgColor: "rgb(31,41,55)", iconColor: "#fff", description: "Apple", icon: <FaApple /> },
+        ].map((circle, index) => (
+          <CircleIcon key={index} {...circle} />
+        ))}
+      </div>
+      <h4 className="mb-2 mt-0 text-3xl font-small leading-tight text-primary px-10 text-start">
+        Popular Meal
+      </h4>
+      <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-0">
+        {[...Array(3)].map((_, index) => (
+          <Popular key={index} />
+        ))}
+      </div>
     </>
-   
-  )
+  );
 }
 
-export default App
+export default App;
